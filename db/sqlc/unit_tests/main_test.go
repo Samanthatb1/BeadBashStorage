517c5f1b6d07db081bd711058f5ef9393f4103ac
Samantha Grieco <samtb1@outlook.com>
2022-07-31 20:15:56 -0400

    added test env

--- db/sqlc/unit_tests/main_test.go
+++ db/sqlc/unit_tests/main_test.go
@@ -9,21 +9,20 @@ import (
 	_ "github.com/lib/pq" // provides the DB driver
 
 	sqlc "github.com/samanthatb1/beadBashStorage/db/sqlc"
-	"github.com/samanthatb1/beadBashStorage/util"
 )
 
 var testQueries *sqlc.Queries
 var testDB *sql.DB
 
-func TestMain(m *testing.M){
-	// Load ENV variables
-	config, err := util.LoadConfig("../../../")
-	if err != nil {
-		log.Fatal("Cannot load configurations (file / env): " , err)
-	}
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://root:secret@localhost:5432/BB-DB?sslmode=disable"
+)
 
+func TestMain(m *testing.M){
+	var err error
 	// Connect to postgres DB
-	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil{ log.Fatal("Cannot connect to db: " , err) }
 	testQueries = sqlc.New(testDB)
 
